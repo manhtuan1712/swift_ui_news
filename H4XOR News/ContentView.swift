94805697f73afd2fe2cac37b888736674a3418f6
tuan.tran <manhtuan17121994@gmail.com>
2021-09-30 15:55:13 +0700

    Initial Commit

---
+++ H4XOR News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by Mập on 30/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
